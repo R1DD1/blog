cc02cec7c58f45762d95e7ee0c771c9796b0c0b3
R1DD1 <biktimirov.oleg@bk.ru>
2023-12-30 16:55:41 +0100

    fix ci/cd

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,31 @@ start=$(date +"%s")
 
 ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
 docker pull moteloff/blog:latest
+docker pull postgres:latest
 
-CONTAINER_NAME=blogapp
-if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
-    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+APP_CONTAINER_NAME=moteloff/blogapp
+if [ "$(docker ps -qa -f name=$APP_CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$APP_CONTAINER_NAME)" ]; then
         echo "Container is running -> stopping it..."
-        docker stop $CONTAINER_NAME;
+        docker stop $APP_CONTAINER_NAME;
     fi
 fi
 
-docker run -d --rm -p 8000:8000 --name $CONTAINER_NAME moteloff/blog:latest
+DB_CONTAINER_NAME=moteloff/postgresdb
+if [ "$(docker ps -qa -f name=$DB_CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$DB_CONTAINER_NAME)" ]; then
+        echo "Container is running -> stopping it..."
+        docker stop $DB_CONTAINER_NAME;
+    fi
+fi
+
+docker run -d --rm -p 5432:5432 --name $DB_CONTAINER_NAME postgres:latest
+docker run -d --rm -p 8080:8080 --name $APP_CONTAINER_NAME moteloff/blog:latest
 
 exit
 ENDSSH
 
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; thenz
   exit 0
 else
   exit 1
